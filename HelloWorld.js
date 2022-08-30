452fca28ee8f5d6fa2d02f80466991c31cb10f8b
Sarmad-Shafique <112169715+Sarmad-Shafique@users.noreply.github.com>
2022-08-30 15:35:44 +0500

    Update HelloWorld.js

--- HelloWorld.js
+++ HelloWorld.js
@@ -6,7 +6,7 @@ const port = 4003;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Potyyy!!!');
+  res.end('Hello World!!!');
 });
 
 server.listen(port, hostname, () => {
